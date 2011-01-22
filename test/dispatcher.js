183d056d1aacd0a1bc4dee998a72e625359383f5
Jon Seymour <jon.seymour@gmail.com>
2011-01-23 03:15:15 +1100

    Initial release of node-cmd - and API for building modular command line utilities with node.js.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

---
+++ test/dispatcher.js
@@ -0,0 +1,88 @@
+/**
+ * (C) copyright Jon Seymour, 2011.
+ */
+var cmd=require("cmd"),
+    assert=require("assert");
+
+module.exports = {
+    "test empty dispatcher, no handler" : function() {
+	var dispatcher = cmd.createDispatcher();
+	assert.throws(function() {
+		dispatcher(["one"]);
+	    });
+    },
+    "test empty dispatcher, no arguments" : function() {
+	var dispatcher = cmd.createDispatcher();
+	dispatcher();
+    },
+    "test empty dispatcher, arbitrary object" : function() {
+	var dispatcher = cmd.createDispatcher();
+	assert.throws(function() {
+		dispatcher({});
+	    });
+    },
+    "test non-empty dispatcher, valid handler" : function() {
+	var dispatcher = cmd.createDispatcher({
+		"method": function (cmd) {
+		    cmd.shared().good = true;
+		    return cmd;
+		}
+	    });
+	var result = dispatcher("method");
+	assert.isDefined(result);
+	assert.isDefined(result.shared());
+	assert.isDefined(result.shared().good);
+	assert.eql(true, result.shared().good);
+    },
+    "test non-empty dispatcher, valid handler, two args" : function() {
+	var dispatcher = cmd.createDispatcher({
+		"method": function (cmd) {
+		    cmd.shared().good = true;
+		    cmd.shared().unparsed = cmd.unparsed();
+		    return cmd;
+		}
+	    });
+	var result = dispatcher("method", "arg");
+	assert.isDefined(result);
+	assert.isDefined(result.shared);
+	assert.isDefined(result.shared());
+	assert.isDefined(result.shared().good);
+	assert.eql(true, result.shared().good);
+	assert.eql(["arg"], result.shared().unparsed);
+    },
+    "test non-empty dispatcher, valid handler - via anonymous function" : function() {
+	var dispatcher = cmd.createDispatcher({
+		"method": function (cmd) {
+		    cmd.shared().good = true;
+		    return cmd;
+		}
+	    });
+	var result = dispatcher("method");
+	assert.isDefined(result);
+	assert.isDefined(result.shared());
+	assert.isDefined(result.shared().good);
+	assert.eql(true, result.shared().good);
+    },
+    "test non-empty dispatcher, object key" : function() {
+	var key={ "a": true  };
+	var config={};
+	config[key]=function(cmd) { return cmd; };
+	var dispatcher = cmd.createDispatcher(config);
+	var result = dispatcher({ "a": true });
+	assert.isDefined(result);
+	assert.type(result, "object");
+	assert.type(result.parsed, "function");
+	assert.isDefined(result.parsed()[0]);
+	assert.eql(key, result.parsed()[0]);
+    },
+    "test nested dispatch" : function() {
+	var dispatcher = cmd.createDispatcher({
+		"outer": cmd.createDispatcher({
+		    "inner": function(x) {
+			return x.unparsed();
+		    }})
+	    });
+	var result = dispatcher("outer", "inner", "args");
+	assert.eql(["args"], result);
+    }
+};
