dd3079f83cbb21c59ba51229aacef87b488cd7d7
Jon Seymour <jon.seymour@gmail.com>
2011-01-23 11:20:45 +1100

    added test/stable.js

---
+++ test/stable.js
@@ -0,0 +1,17 @@
+/**
+ * (C) cmd - Copyright Jon Seymour, 2001
+ */
+
+/**
+ * This test will document specified behaviour of the API. 
+ *
+ * Unless otherwise specified, breaking changes to the specified behaviour will be marked
+ * with a change to major version number.
+ */
+
+var cmd=require("cmd");
+
+module.exports = {
+    "always true": function() {
+    }
+}
