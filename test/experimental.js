1941cb8c30eeb1e941e0f5a208b5a8eaea48b35d
Jon Seymour <jon.seymour@gmail.com>
2011-01-29 21:52:34 +1100

    Add some dispatching tests.

--- test/experimental.js
+++ test/experimental.js
@@ -3,13 +3,36 @@
  */
 
 /**
- * This test will document proposed behaviour of the API. 
+ * This test will document proposed behaviour of the API.
  *
  * Any behaviours documented by this file are subject to change without notice.
  */
 
 var CMD=require("cmd"),
-    assert=require("assert");
+    assert=require("assert"),
+    DISPATCHER=CMD.createDispatcher({
+		foo : function(cmd) {
+		    return {
+			subject: this,
+			arguments: arguments,
+			cmd: cmd
+		    };
+	        },
+		bar : function(cmd) {
+		    return {
+			subject: this,
+			arguments: arguments,
+			cmd: cmd
+		    };
+	        },
+		_unhandled : function(cmd) {
+		    return {
+			subject: this,
+			arguments: arguments,
+			cmd: cmd
+		    };
+		},
+	});
 
 function assertCommandInvariant(aCmd) {
     assert.isDefined(aCmd);
@@ -28,6 +51,30 @@ function assertCommandInvariant(aCmd) {
 
 }
 
+function assertEmptyCommand(cmd) {
+    assert.eql(cmd.options, {});
+    assert.eql(cmd.unshifted(), []);
+    assert.eql(cmd.shifted(), []);
+    assert.eql(cmd.shared(), {});
+}
+
+function assertExpectedResult(expected, dispatcher, result) {
+    assert.isDefined(dispatcher);
+    assert.type(dispatcher, "function");
+    assertCommandInvariant(result.cmd);
+    assert.equal(dispatcher, result.subject.api);
+    assert.length(result.arguments, 1);
+    assert.equal(result.cmd, result.arguments[0]);
+    assert.eql(expected.shifted, result.cmd.shifted());
+    assert.eql(expected.unshifted, result.cmd.unshifted());
+    assert.eql(expected.options, result.cmd.options);
+}
+
+function test(input, expected) {
+    var result = DISPATCHER(input);
+    assertExpectedResult(expected, DISPATCHER, result);
+}
+
 module.exports = {
     "createDispatcher exists": function() {
 	assert.isDefined(CMD.createDispatcher);
@@ -40,15 +87,54 @@ module.exports = {
 	assert.type(CMD.Command, "function");
     },
     "createCommand() yields an instanceof Command with no unshifted arguments": function() {
-
 	var aCmd=CMD.createCommand();
-
 	assertCommandInvariant(aCmd);
-
-	assert.eql(aCmd.options, {});
-
-	assert.eql(aCmd.unshifted(), []);
-	assert.eql(aCmd.shifted(), []);
-	assert.eql(aCmd.shared(), {});
+	assertEmptyCommand(aCmd);
+    },
+    "createDispatcher() yields a function": function() {
+	var dispatcher=CMD.createDispatcher({ });
+	assert.isDefined(dispatcher);
+	assert.type(dispatcher, "function");
+    },
+    "createDispatcher() yields a function - unhandled": function() {
+	var dispatcher=CMD.createDispatcher({
+		_unhandled : function(cmd) {
+		    return [ this,cmd ];
+		}
+	    });
+	assert.isDefined(dispatcher);
+	assert.type(dispatcher, "function");
+	var result = dispatcher();
+	assertCommandInvariant(result[1]);
+	assertEmptyCommand(result[1]);
+	assert.equal(dispatcher, result[0].api);
+    },
+    "test dispatch: <empty>": function() {
+	test([], {
+		 shifted: [ ],
+		 unshifted: [],
+		 options: {}
+	     });
+    },
+    "test dispatch: foo": function() {
+	test(["foo"], {
+		 shifted: [ "foo" ],
+		 unshifted: [],
+		 options: {}
+	     });
+    },
+    "test dispatch: foo bar": function() {
+	test(["foo", "bar"], {
+		 shifted: [ "foo" ],
+		 unshifted: [ "bar" ],
+		 options: {}
+	     });
+    },
+    "test dispatch: baz": function() {
+	test(["baz"], {
+		 shifted: [ ],
+		 unshifted: [ "baz" ],
+		 options: {}
+	     });
     }
 }
